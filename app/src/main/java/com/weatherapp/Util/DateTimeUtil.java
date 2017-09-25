e94fbffc48d1fdfa1f250462cc199001e3bcc024
Josh annang <>
2017-09-25 11:09:32 +0100

    Built DateTimeUtil class.

---
+++ app/src/main/java/com/weatherapp/Util/DateTimeUtil.java
@@ -0,0 +1,24 @@
+package com.weatherapp.Util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by Xpieon on 25/09/2017.
+ */
+
+public class DateTimeUtil {
+
+    public static String getDay(Date date){
+        DateFormat format = new SimpleDateFormat("EEEEEEEEE", Locale.ENGLISH);
+        return format.format(date);
+    }
+
+    public static String getTime(Date date){
+        DateFormat format = new SimpleDateFormat("hh-aa", Locale.ENGLISH);
+        return format.format(date);
+    }
+
+}
